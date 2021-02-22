b6ac968778093d7c3b61efc55d92fce8eb71641a
Heather Kusma <heatherkusma@heathers-air.quinnipiac.edu>
2021-02-21 20:10:21 -0500

    changed buttons to yellow

--- src/Snowman.java
+++ src/Snowman.java
@@ -61,17 +61,17 @@ public class Snowman extends Frame{
 		button1 = new Ellipse();
 		button1.setLocation(355,245);
 		button1.setSize(15,15);
-		button1.setColor(Color.blue);
+		button1.setColor(Color.yellow);
 		
 		button2 = new Ellipse();
 		button2.setLocation(355,275);
 		button2.setSize(15,15);
-		button2.setColor(Color.black);
+		button2.setColor(Color.yellow);
 		
 		button3 = new Ellipse();
 		button3.setLocation(355,305);
 		button3.setSize(15,15);
-		button3.setColor(Color.red);
+		button3.setColor(Color.yellow);
 		
 		
 		hat = new Rectangle();
